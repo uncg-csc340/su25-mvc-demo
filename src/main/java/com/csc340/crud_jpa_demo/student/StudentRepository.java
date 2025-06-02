624fb52a1fe7d1f6f9bcb00a09536a45c8671c5f
uncg-csc340 <csc340sp24@gmail.com>
2025-06-02 01:35:05 -0400

    Convert to MVC Controller, add first views

---
+++ src/main/java/com/csc340/crud_jpa_demo/student/StudentRepository.java
@@ -0,0 +1,18 @@
+package com.csc340.crud_jpa_demo.student;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> getStudentsByMajor(String major);
+
+    @Query(value = "select * from students s where s.gpa >= ?1", nativeQuery = true)
+    List<Student> getHonorsStudents(double gpa);
+
+    @Query(value = "select * from students s where s.name like %?1% ", nativeQuery = true)
+    List<Student> getStudentsByName(String name);
+}
